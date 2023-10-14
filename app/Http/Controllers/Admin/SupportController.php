ef4981712767c76bba9e206343aa8f0000ef3748
Matheus Duarte <matheusduartegalvao@gmail.com>
2023-10-14 16:02:38 -0300

    feat: adding update and delete for supports

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -15,6 +15,15 @@ class SupportController extends Controller
         return view('admin/supports/index', compact('supports'));
     }
 
+    public function show(string|int $id)
+    {
+        if(!$support = Support::find($id)) {
+            return back();
+        }
+
+        return view('admin/supports/show', compact('support'));
+    }
+
     public function create()
     {
        return view('admin/supports/create');
@@ -26,6 +35,39 @@ class SupportController extends Controller
         $data['status'] = 'a';
 
         $support = $support->create($data);
+
+        return redirect()->route('supports.index');
+    }
+
+    public function edit(Support $support, string|int $id)
+    {
+        if(!$support = $support->where('id',$id)->first()) {
+            return back();
+        }
+
+        return view('admin/supports/edit', compact('support'));
+    }
+
+    public function update(Request $request, Support $support, string $id)
+    {
+        if(!$support = $support->find($id)) {
+            return back();
+        }
+
+        $support->update($request->only([
+            'subject', 'body'
+        ]));
+
+        return redirect()->route('supports.index');
+    }
+
+    public function destroy(string|int $id)
+    {
+        if(!$support = Support::find($id)) {
+            return back();
+        }
+
+        $support->delete();
 
         return redirect()->route('supports.index');
     }
