7c79db9d6d79db43da3c59c639843d9c160a974c
Matheus Duarte <matheusduartegalvao@gmail.com>
2023-10-15 15:21:31 -0300

    feat: finishing visual adjusts

--- resources/views/admin/supports/show.blade.php
+++ resources/views/admin/supports/show.blade.php
@@ -1,15 +1,35 @@
-<h1>Detalhes da dúvida {{ $support->id }}</h1>
+@extends('admin.layouts.app')
 
-<ul>
-    <li>Assunto: {{ $support->subject }}</li>
-    <li>Descrição: {{ $support->body }}</li>
-    <li>Status: {{ $support->status }}</li>
-</ul>
+@section('title', "Detalhes da Dúvida {$support->subject}")
 
-<form action="{{ route('supports.destroy', $support->id) }}" method="post">
-    @csrf()
-    @method('DELETE')
-    <button type="submit">Deletar</button>
-</form>
+@section('header')
+<h1 class="text-lg text-black-500">Dúvida {{ $support->subject }}</h1>
+@endsection
+
+@section('content')
+
+
+<div class="max-w-sm rounded overflow-hidden shadow-lg">
+    <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">Assunto: {{ $support->subject }}</div>
+        <p class="text-gray-700 text-base">
+            {{ $support->body }}
+        </p>
+        <p class="text-gray-700 text-base">
+            Status: {{  getStatusSupport($support->status) }}
+        </p>
+        <p class="text-gray-700 text-base my-4">
+            <form action="{{ route('supports.destroy', $support->id) }}" method="post">
+                @csrf()
+                @method('DELETE')
+                <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" type="submit">Deletar</button>
+            </form>
+        </p>
+    </div>
+</div>
+
+
+
+@endsection
 
 
